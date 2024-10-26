feeeaee99906d18a0bc1e92259b52baa4524502c
hlimhli <34466635+hlimhli@users.noreply.github.com>
2024-10-26 15:45:31 +0800

    added search query

--- js/server.js
+++ js/server.js
@@ -12,12 +12,19 @@ require("handlebars-helpers")({
   handlebars: hbs.handlebars
 });
 
-// handlebars-helper not working?
-hbs.registerHelper('if_eq', function(a, b, opts) {
-  if(a == b)
-      return opts.fn(this);
+// example of defining helpers, actual handlebars-helper function for it is 'eq'
+hbs.registerHelper('if_eq', function (a, b, opts) {
+  if (a == b)
+    return opts.fn(this);
   else
-      return opts.inverse(this);
+    return opts.inverse(this);
+});
+
+hbs.registerHelper('if_notUndefNull', function (val, opts) {
+  if (val === null || val === undefined)
+    return opts.inverse(this);
+  else
+    return opts.fn(this);
 });
 
 //allow serving static files
@@ -36,30 +43,30 @@ wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
 function dateToMySqlDate(date) {
-  if(!date || isNaN(date.getTime())) return undefined;
+  if (!date || isNaN(date.getTime())) return undefined;
   return `${date.getFullYear()}-` +
-         `${String(date.getMonth()+1).padStart(2,'0')}-` +
-         `${String(date.getDate()).padStart(2,'0')}`;
+    `${String(date.getMonth() + 1).padStart(2, '0')}-` +
+    `${String(date.getDate()).padStart(2, '0')}`;
 }
 
 function dateToMySqlDatetime(date) {
-  return `${dateToMySqlDate(date)} `+
-         `${String(date.getHours()).padStart(2,'0')}:` +
-         `${String(date.getMinutes()).padStart(2,'0')}:` +
-         `${String(date.getSeconds()).padStart(2,'0')}`;
+  return `${dateToMySqlDate(date)} ` +
+    `${String(date.getHours()).padStart(2, '0')}:` +
+    `${String(date.getMinutes()).padStart(2, '0')}:` +
+    `${String(date.getSeconds()).padStart(2, '0')}`;
 }
 
 function dateToHtmlDate(date) {
-  if(!date || isNaN(date.getTime())) return undefined;
+  if (!date || isNaN(date.getTime())) return undefined;
   return `${date.getFullYear()}` +
-         `-${String(date.getMonth()).padStart(2, 0)}` +
-         `-${String(date.getDate()).padStart(2,0)}`;
+    `-${String(date.getMonth()).padStart(2, 0)}` +
+    `-${String(date.getDate()).padStart(2, 0)}`;
 }
 
 function dateToHtmlDatetime(date) {
   return `${dateToHtmlDate(date)}` +
-         `T${String(date.getHours()).padStart(2,'0')}` + 
-         `:${String(date.getMinutes()).padStart(2,'0')}`;
+    `T${String(date.getHours()).padStart(2, '0')}` +
+    `:${String(date.getMinutes()).padStart(2, '0')}`;
 }
 
 async function main() {
@@ -105,7 +112,7 @@ async function main() {
       FROM customers
     `);
     res.render("customers", {
-      "customers" : customers
+      "customers": customers
     });
   });
 
@@ -136,8 +143,8 @@ async function main() {
   });
 
   app.post("/create", async (req, res) => {
-    const {name, address, email, template_id, material_id, remarks} = req.body;
-    let {contact} = req.body;
+    const { name, address, email, template_id, material_id, remarks } = req.body;
+    let { contact } = req.body;
     const numbers = contact.match(/\d+/g);
     contact = numbers.join('');
 
@@ -149,15 +156,15 @@ async function main() {
 
     let customerId;
 
-    if(customers.length < 1) {
+    if (customers.length < 1) {
       const query = `
         INSERT INTO customers (name, contact, email, address)
         VALUES (?, ?, ?, ?);
       `;
 
-      const [result] = 
+      const [result] =
         await connection.execute(query, [name, contact, email, address]);
-      customerId =  result.insertId;
+      customerId = result.insertId;
       //  await 
     } else { // email must be unique
       customerId = customers[0].customer_id;
@@ -167,7 +174,7 @@ async function main() {
       SELECT smith_id
       FROM smiths;
     `);
-    
+
     const smith_id = smiths[Math.floor(Math.random() * smiths.length)].smith_id;
 
     const insertQuery = `
@@ -178,25 +185,25 @@ async function main() {
     `;
 
     await connection.execute(insertQuery,
-      [ dateToMySqlDatetime(new Date()),
-        dateToMySqlDate(new Date(Date.now() + 2592000000)),
+      [dateToMySqlDatetime(new Date()),
+      dateToMySqlDate(new Date(Date.now() + 2592000000)),
         customerId, smith_id, template_id, material_id, remarks
       ]);
 
     res.redirect("/read");
   });
 
-  app.get("/update", async (req, res)=>{
+  app.get("/update", async (req, res) => {
     const [orders] = await connection.execute(`
       SELECT order_id
       FROM orders
     ;`);
 
-    const {order_id} = req.query;
+    const { order_id } = req.query;
 
-    const data = { "orders" : orders };
+    const data = { "orders": orders };
 
-    if(order_id) {
+    if (order_id) {
       const [order] = (await connection.execute(`
         SELECT *
         FROM orders
@@ -205,7 +212,7 @@ async function main() {
 
       order.posted = dateToHtmlDatetime(order.posted);
       order.complete_by = dateToHtmlDate(order.complete_by);
-      if(order.completed_at) {
+      if (order.completed_at) {
         order.completed_at = dateToHtmlDatetime(order.completed_at);
       } else {
         order.completed_at = dateToHtmlDatetime(new Date(0));
@@ -243,7 +250,7 @@ async function main() {
 
   app.post("/update", async (req, res) => {
     const { complete_by, smith_id, template_id, order_id,
-            material_id, customer_id, remarks } = req.body;
+      material_id, customer_id, remarks } = req.body;
     const completed_at =
       req.body.completed_at.length == 0 ? null : req.body.completed_at;
 
@@ -253,9 +260,9 @@ async function main() {
                        remarks = ?
                    WHERE order_id = ?;`;
     const values = [
-      dateToMySqlDate(new Date(complete_by)), smith_id, 
-      template_id, material_id, customer_id, 
-      completed_at ? dateToMySqlDatetime(new Date(completed_at)): null,
+      dateToMySqlDate(new Date(complete_by)), smith_id,
+      template_id, material_id, customer_id,
+      completed_at ? dateToMySqlDatetime(new Date(completed_at)) : null,
       remarks, order_id
     ];
     await connection.execute(query, values);
@@ -281,7 +288,7 @@ async function main() {
       WHERE order_id = ${order_id};
     `))[0];
 
-    res.render("delete", {"order" : order});
+    res.render("delete", { "order": order });
   });
 
   app.post("/delete/:order_id", async (req, res) => {
@@ -293,7 +300,48 @@ async function main() {
     await connection.execute(query, [order_id]);
     res.redirect("../read");
   });
-};
+
+  app.get("/search", async (req, res) => {
+    const { template_id, material_id } = req.query;
+    const [materials] = await connection.execute(`
+        SELECT material_id, name
+        FROM materials;
+    `);
+    const [templates] = await connection.execute(`
+      SELECT template_id, name
+      FROM templates;
+    `);
+    let results = undefined;
+    if (template_id && material_id) {
+      [results] = (await connection.execute(`
+        SELECT order_id,
+                posted,
+                complete_by,
+                completed_at,
+                smiths.name AS smithname,
+                templates.name AS templatename,
+                materials.name AS materialname,
+                customer_id,
+                remarks
+        FROM orders 
+          JOIN smiths ON orders.smith_id = smiths.smith_id
+          JOIN templates ON orders.template_id = templates.template_id
+          JOIN materials ON orders.material_id = materials.material_id
+        WHERE templates.template_id = ${template_id} AND materials.material_id = ${material_id}
+      `));
+    }
+
+    let data = {
+      "materials": materials,
+      "templates": templates,
+      "results": results,
+      "selectedTemplate" : template_id === undefined ? -1 : template_id ,
+      "selectedMaterial" : material_id === undefined ? -1 : material_id
+    }
+    console.log(data);
+    res.render("search_get", data);
+  });
+}
 
 main();
 
